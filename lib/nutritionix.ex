8d31d7bde13ebce7cb1c84ae01d21eaaac8a1f68
Jon Canady <jon@prescribefit.com>
2022-06-22 10:16:26 -0400

    mix new nutritionix

---
+++ lib/nutritionix.ex
@@ -0,0 +1,18 @@
+defmodule Nutritionix do
+  @moduledoc """
+  Documentation for `Nutritionix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nutritionix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nutritionix_test.exs
@@ -0,0 +1,8 @@
+defmodule NutritionixTest do
+  use ExUnit.Case
+  doctest Nutritionix
+
+  test "greets the world" do
+    assert Nutritionix.hello() == :world
+  end
+end
