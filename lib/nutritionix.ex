b8bcfa098bc7ec973ba49cecc3a1862c1acfd683
Jon Canady <jon@prescribefit.com>
2022-06-24 17:18:14 -0400

    Add bang/non-bang Nutritionix.client/1 functions.

--- lib/nutritionix.ex
+++ lib/nutritionix.ex
@@ -4,23 +4,46 @@ defmodule Nutritionix do
 
   https://www.nutritionix.com/ for more details.
   """
+  @nutritionix_api_baseurl "https://trackapi.nutritionix.com"
 
-  # Get a client for the Nutritionix service.
-  #
-  # Clients require a user identifier to distinguish one user from another.
-  # This identifer can be anything, so long as it is consistent.
-  #
+  @doc """
+  Get a client for the Nutritionix service.
+
+  The Nutritionix API requires a user identifier to distinguish one user from
+  another.
+
   ## Examples
-  #
-  # iex> Nutritionix.client("userid")
-  # Tesla.client{}
-  #
-  @spec client(String.t()) :: Tesla.Client.t()
+
+      iex> {:ok, %Tesla.Client{}} = Nutritionix.client("userid")
+  """
+  @spec client(String.t()) :: {:ok, Tesla.Client.t()}
   def client(user_id) do
+    try do
+      {:ok, client!(user_id)}
+    rescue
+      e ->
+        {:error, e.message}
+    end
+  end
+
+  @doc """
+  Get a client for the Nutritionix service (directly, no tuple)
+
+  See client/1 for the function you probably want.
+
+  ## Examples
+
+      iex> %Tesla.Client{} = Nutritionix.client!("userid")
+  """
+  @spec client!(String.t()) :: Tesla.Client.t()
+  def client!(user_id) do
+    app_id = System.fetch_env!("NUTRITIONIX_APP_ID")
+    app_key = System.fetch_env!("NUTRITIONIX_APP_KEY")
+
     middleware = [
-      {Tesla.Middleware.BaseUrl, "https://trackapi.nutritionix.com"},
+      {Tesla.Middleware.BaseUrl, @nutritionix_api_baseurl},
       {Tesla.Middleware.Headers,
-       [{"x-app-id", ""}, {"x-app-key", ""}, {"x-remote-user-id", user_id}]},
+       [{"x-app-id", app_id}, {"x-app-key", app_key}, {"x-remote-user-id", user_id}]},
       Tesla.Middleware.JSON
     ]
 
--- test/nutritionix_test.exs
+++ test/nutritionix_test.exs
@@ -1,11 +1,17 @@
 defmodule NutritionixTest do
   use ExUnit.Case
+
+  setup do
+    System.put_env("NUTRITIONIX_APP_KEY", "apikey")
+    System.put_env("NUTRITIONIX_APP_ID", "apiid")
+  end
+
   doctest Nutritionix
 
   describe "client/1" do
-    test "returns a Tesla client" do
-      client = Nutritionix.client("fiddytwo")
-      assert %Tesla.Client{} = client
+    test "returns errors nicely isntead of raising exceptions" do
+      System.delete_env("NUTRITIONIX_APP_KEY")
+      assert {:error, _message} = Nutritionix.client("userid")
     end
   end
 end
